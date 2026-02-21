b3544e5afd3e6749727ce279bd9d1d1307ce881d
xylin <xylin.dev@qq.com>
2026-02-21 18:33:54 +0800

    cleanup
    cleanup

---
+++ site1/test_app.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pytest
+
+from site1.accounts import find_account_by_username
+
+
+@pytest.fixture
+def client():
+   from .app import app
+   return app.test_client()
+
+
+def test_transfer(client):
+   alice_token = Path(__file__).parent.joinpath('token_alice').read_text()
+   alice = find_account_by_username('alice')
+
+   client.set_cookie("token", alice_token)
+   resp = client.post('/accounts/transfer', data={'to': 'bob', 'amount': 100},
+                      headers={"Cookie": f"token={alice_token}"}, )
+   assert resp.status_code == 200
+   assert alice.balance == 900
